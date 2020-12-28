61feff4163cd2ce09e3d321a1ff18bd2656420f7
alexander-p30 <alexanderp30e@gmail.com>
2020-12-28 19:10:59 -0300

    :white_check_mark: Updates test for product_list_controller

--- test/baggy_backend_web/controllers/api/v1/product_list_controller_test.exs
+++ test/baggy_backend_web/controllers/api/v1/product_list_controller_test.exs
@@ -1,21 +1,13 @@
 defmodule BaggyBackendWeb.Api.V1.ProductListControllerTest do
   use BaggyBackendWeb.ConnCase
 
-  alias BaggyBackend.Products
   alias BaggyBackend.Products.ProductList
-  alias BaggyBackend.Houses
 
-  @house_valid_attrs %{code: "a45bn0", name: "My House", passcode: "531361"}
-  @update_attrs %{
-    name: "some updated name"
-  }
-  @invalid_attrs %{name: nil}
+  import BaggyBackend.Fixture
 
-  def fixture(:product_list) do
-    {:ok, house} = Houses.create_house(@house_valid_attrs)
-    {:ok, product_list} = Products.create_product_list(%{name: "some name", house_id: house.id})
-    product_list
-  end
+  @create_attrs attrs(:list, :valid_attrs)
+  @update_attrs attrs(:list, :update_attrs)
+  @invalid_attrs attrs(:list, :invalid_attrs)
 
   setup %{conn: conn} do
     {:ok, conn: put_req_header(conn, "accept", "application/json")}
@@ -30,20 +22,27 @@ defmodule BaggyBackendWeb.Api.V1.ProductListControllerTest do
 
   describe "create product_list" do
     test "renders product_list when data is valid", %{conn: conn} do
-      {:ok, house} = Houses.create_house(@house_valid_attrs)
-      conn = post(conn, Routes.api_v1_product_list_path(conn, :create), product_list: %{name: "some name", house_id: house.id})
+      house = fixture(:house, :valid_attrs)
+
+      create_attrs = Map.merge(@create_attrs, %{house_id: house.id})
+
+      conn =
+        post(conn, Routes.api_v1_product_list_path(conn, :create), product_list: create_attrs)
+
       assert %{"id" => id} = json_response(conn, 201)["data"]
 
       conn = get(conn, Routes.api_v1_product_list_path(conn, :show, id))
 
       assert %{
                "id" => _id,
-               "name" => "some name"
+               "name" => "Churras"
              } = json_response(conn, 200)["data"]
     end
 
     test "renders errors when data is invalid", %{conn: conn} do
-      conn = post(conn, Routes.api_v1_product_list_path(conn, :create), product_list: @invalid_attrs)
+      conn =
+        post(conn, Routes.api_v1_product_list_path(conn, :create), product_list: @invalid_attrs)
+
       assert json_response(conn, 422)["errors"] != %{}
     end
   end
@@ -51,20 +50,31 @@ defmodule BaggyBackendWeb.Api.V1.ProductListControllerTest do
   describe "update product_list" do
     setup [:create_product_list]
 
-    test "renders product_list when data is valid", %{conn: conn, product_list: %ProductList{id: id} = product_list} do
-      conn = put(conn, Routes.api_v1_product_list_path(conn, :update, product_list), product_list: @update_attrs)
+    test "renders product_list when data is valid", %{
+      conn: conn,
+      product_list: %ProductList{id: id} = product_list
+    } do
+      conn =
+        put(conn, Routes.api_v1_product_list_path(conn, :update, product_list),
+          product_list: Map.take(@update_attrs, ["name"])
+        )
+
       assert %{"id" => ^id} = json_response(conn, 200)["data"]
 
       conn = get(conn, Routes.api_v1_product_list_path(conn, :show, id))
 
       assert %{
                "id" => _id,
-               "name" => "some updated name"
+               "name" => "Churras"
              } = json_response(conn, 200)["data"]
     end
 
     test "renders errors when data is invalid", %{conn: conn, product_list: product_list} do
-      conn = put(conn, Routes.api_v1_product_list_path(conn, :update, product_list), product_list: @invalid_attrs)
+      conn =
+        put(conn, Routes.api_v1_product_list_path(conn, :update, product_list),
+          product_list: @invalid_attrs
+        )
+
       assert json_response(conn, 422)["errors"] != %{}
     end
   end
@@ -83,7 +93,6 @@ defmodule BaggyBackendWeb.Api.V1.ProductListControllerTest do
   end
 
   defp create_product_list(_) do
-    product_list = fixture(:product_list)
-    %{product_list: product_list}
+    %{product_list: fixture(:list, :valid_attrs)}
   end
 end
