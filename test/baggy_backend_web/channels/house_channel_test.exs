4c2c128d025aec48c731dfc4d574c61600642539
alexander-p30 <alexanderp30e@gmail.com>
2021-01-11 15:55:43 -0300

    :white_check_mark: Tests for HouseChannel crud for product

--- test/baggy_backend_web/channels/house_channel_test.exs
+++ test/baggy_backend_web/channels/house_channel_test.exs
@@ -1,12 +1,12 @@
 defmodule BaggyBackendWeb.HouseChannelTest do
   use BaggyBackendWeb.ChannelCase
 
-  alias BaggyBackend.Products.ProductList
+  alias BaggyBackend.Products.{ProductList, Product}
 
   import BaggyBackend.Fixture
 
   describe "join" do
-    test "joining with a house with no lists return an empty list under product_lists" do
+    test "joining with a house with no lists returns it and and empty list for its product_lists" do
       house = fixture(:house, :valid_attrs)
 
       {:ok, response, _} =
@@ -14,10 +14,12 @@ defmodule BaggyBackendWeb.HouseChannelTest do
         |> socket("user_id", %{some: :assign})
         |> subscribe_and_join(BaggyBackendWeb.HouseChannel, "house:#{to_string(house.id)}")
 
-      assert response == %{product_lists: []}
+      house = BaggyBackend.Repo.preload(house, :product_lists)
+
+      assert response == %{house: house, product_lists: []}
     end
 
-    test "joining with a house with lists return it's product lists" do
+    test "joining a house with prodduct lists returns it and it's product lists" do
       house = fixture(:house, :valid_attrs)
 
       product_lists =
@@ -28,12 +30,14 @@ defmodule BaggyBackendWeb.HouseChannelTest do
           })
         end)
 
+      house = BaggyBackend.Repo.preload(house, :product_lists)
+
       {:ok, response, _} =
         BaggyBackendWeb.UserSocket
         |> socket("user_id", %{some: :assign})
         |> subscribe_and_join(BaggyBackendWeb.HouseChannel, "house:#{to_string(house.id)}")
 
-      assert response == %{product_lists: product_lists}
+      assert response == %{house: house, product_lists: product_lists}
     end
   end
 
@@ -61,7 +65,7 @@ defmodule BaggyBackendWeb.HouseChannelTest do
         "product_list" => %{name: "New name"}
       })
 
-      assert_broadcast "product_list:updated", %{
+      assert_broadcast "product_list:update", %{
         product_list: %ProductList{name: "New name", id: _product_list_id}
       }
     end
@@ -140,31 +144,211 @@ defmodule BaggyBackendWeb.HouseChannelTest do
     end
   end
 
-  describe "handle_in" do
+  describe "handle_in product_list:product:create" do
     setup do
-      house = fixture(:house, :valid_attrs)
+      house = fixture(:house, :valid_attrs, %{code: "housetst"})
+      product_list = fixture(:product_list, :valid_attrs, %{house_id: house.id})
+      product_category = fixture(:product_category, :valid_attrs)
+      user = fixture(:user, :valid_attrs)
+
+      fixture(:houses_users, :valid_attrs, %{
+        house_id: house.id,
+        user_uuid: user.uuid,
+        is_owner: true
+      })
 
       {:ok, _, socket} =
         BaggyBackendWeb.UserSocket
         |> socket("user_id", %{some: :assign})
         |> subscribe_and_join(BaggyBackendWeb.HouseChannel, "house:#{to_string(house.id)}")
 
-      %{socket: socket}
+      %{
+        socket: socket,
+        product_list: product_list,
+        product_category: product_category,
+        user: user
+      }
     end
 
-    test "ping replies with status ok", %{socket: socket} do
-      ref = push(socket, "ping", %{"hello" => "there"})
-      assert_reply ref, :ok, %{"hello" => "there"}
+    test "broadcasts created product and it's product list when params are valid", %{
+      socket: socket,
+      product_list: product_list,
+      product_category: product_category,
+      user: user
+    } do
+      product_list_id = product_list.id
+
+      product_attrs =
+        attrs(:product, :valid_attrs)
+        |> Map.merge(%{
+          product_list_id: product_list_id,
+          product_category_id: product_category.id,
+          user_uuid: user.uuid
+        })
+
+      push(socket, "product_list:product:create", %{
+        "product_list_id" => product_list_id,
+        "product" => product_attrs
+      })
+
+      assert_broadcast "product_list:product:create", %{
+        product_list_id: _product_list_id,
+        product: %Product{}
+      }
     end
 
-    test "shout broadcasts to house:lobby", %{socket: socket} do
-      push(socket, "shout", %{"hello" => "all"})
-      assert_broadcast "shout", %{"hello" => "all"}
+    test "replies with list of error tuples when params are invalid", %{
+      socket: socket,
+      product_list: product_list
+    } do
+      product_attrs = attrs(:product, :invalid_attrs)
+
+      ref =
+        push(socket, "product_list:product:create", %{
+          "product_list_id" => product_list.id,
+          "product" => product_attrs
+        })
+
+      assert_reply ref, :error, [
+        {:name, {"can't be blank", [validation: :required]}},
+        {:product_list_id, {"can't be blank", [validation: :required]}},
+        {:product_category_id, {"can't be blank", [validation: :required]}},
+        {:user_uuid, {"can't be blank", [validation: :required]}}
+      ]
+    end
+  end
+
+  describe "handle_in product_list:product:update" do
+    setup do
+      house = fixture(:house, :valid_attrs, %{code: "housetst"})
+      product_list = fixture(:product_list, :valid_attrs, %{house_id: house.id})
+      product_category = fixture(:product_category, :valid_attrs)
+      user = fixture(:user, :valid_attrs)
+
+      assocs = %{
+        product_list_id: product_list.id,
+        product_category_id: product_category.id,
+        user_uuid: user.uuid
+      }
+
+      product = fixture(:product, :valid_attrs, assocs)
+
+      fixture(:houses_users, :valid_attrs, %{
+        house_id: house.id,
+        user_uuid: user.uuid,
+        is_owner: true
+      })
+
+      {:ok, _, socket} =
+        BaggyBackendWeb.UserSocket
+        |> socket("user_id", %{some: :assign})
+        |> subscribe_and_join(BaggyBackendWeb.HouseChannel, "house:#{to_string(house.id)}")
+
+      %{
+        socket: socket,
+        product_id: product.id,
+        product_list_id: product_list.id
+      }
     end
 
-    test "broadcasts are pushed to the client", %{socket: socket} do
-      broadcast_from!(socket, "broadcast", %{"some" => "data"})
-      assert_push "broadcast", %{"some" => "data"}
+    test "broadcasts updated product and it's product list id when params are valid", %{
+      socket: socket,
+      product_id: product_id,
+      product_list_id: _product_list_id
+    } do
+      product_attrs = attrs(:product, :update_attrs)
+
+      push(socket, "product_list:product:update", %{
+        "id" => product_id,
+        "product" => product_attrs
+      })
+
+      assert_broadcast "product_list:product:update", %{
+        product_list_id: _product_list_id,
+        product: %Product{}
+      }
+    end
+
+    test "replies with list of error tuples when params are invalid", %{
+      socket: socket,
+      product_id: product_id
+    } do
+      product_attrs = attrs(:product, :invalid_attrs)
+
+      ref =
+        push(socket, "product_list:product:update", %{
+          "id" => product_id,
+          "product" => product_attrs
+        })
+
+      assert_reply ref, :error, [
+        {:name, {"can't be blank", [validation: :required]}}
+      ]
+    end
+  end
+
+  describe "handle_in product_list:product:delete" do
+    setup do
+      house = fixture(:house, :valid_attrs, %{code: "housetst"})
+      product_list = fixture(:product_list, :valid_attrs, %{house_id: house.id})
+      product_category = fixture(:product_category, :valid_attrs)
+      user = fixture(:user, :valid_attrs)
+
+      assocs = %{
+        product_list_id: product_list.id,
+        product_category_id: product_category.id,
+        user_uuid: user.uuid
+      }
+
+      product = fixture(:product, :valid_attrs, assocs)
+
+      fixture(:houses_users, :valid_attrs, %{
+        house_id: house.id,
+        user_uuid: user.uuid,
+        is_owner: true
+      })
+
+      {:ok, _, socket} =
+        BaggyBackendWeb.UserSocket
+        |> socket("user_id", %{some: :assign})
+        |> subscribe_and_join(BaggyBackendWeb.HouseChannel, "house:#{to_string(house.id)}")
+
+      %{
+        socket: socket,
+        product_id: product.id,
+        product_list_id: product_list.id
+      }
+    end
+
+    test "broadcasts deleted product id and it's product list id when id is valid", %{
+      socket: socket,
+      product_id: product_id,
+      product_list_id: _product_list_id
+    } do
+      push(socket, "product_list:product:delete", %{
+        "id" => product_id
+      })
+
+      assert_broadcast "product_list:product:delete", %{
+        product_list_id: _product_list_id,
+        product_id: _product_id
+      }
+    end
+
+    test "replies with list of error tuples when id is invalid", %{
+      socket: socket,
+      product_id: product_id
+    } do
+      invalid_id = product_id + 1
+
+      ref =
+        push(socket, "product_list:product:delete", %{
+          "id" => invalid_id
+        })
+
+      _message = "Record with id #{invalid_id} could not be found"
+
+      assert_reply ref, :error, _message
     end
   end
 end
