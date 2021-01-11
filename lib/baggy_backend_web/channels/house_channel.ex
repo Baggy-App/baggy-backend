cc37fe9cb792ed8f43111803b23d696bafdb9c7a
alexander-p30 <alexanderp30e@gmail.com>
2021-01-11 15:55:25 -0300

    :sparkles: HouseChannel crud for product

--- lib/baggy_backend_web/channels/house_channel.ex
+++ lib/baggy_backend_web/channels/house_channel.ex
@@ -4,7 +4,7 @@ defmodule BaggyBackendWeb.HouseChannel do
   """
   use BaggyBackendWeb, :channel
 
-  alias BaggyBackend.{Houses, Products, Products.ProductList}
+  alias BaggyBackend.{Houses, Products, Products.ProductList, Products.Product}
 
   alias BaggyBackendWeb.ParamsHandler, as: Params
 
@@ -12,44 +12,51 @@ defmodule BaggyBackendWeb.HouseChannel do
 
   @product_list_create_attrs ["name", "house_id"]
 
+  @product_create_attrs [
+    "name",
+    "description",
+    "quantity",
+    "min_price",
+    "max_price",
+    "done",
+    "product_list_id",
+    "product_category_id",
+    "user_uuid"
+  ]
+
+  @product_update_attrs [
+    "name",
+    "description",
+    "quantity",
+    "min_price",
+    "max_price",
+    "done"
+  ]
+
   @impl true
   def join("house:" <> house_id, payload, socket) do
-    if authorized?(payload) do
-      %{product_lists: product_lists} = Houses.get_house!(house_id, :product_lists)
+    with true <- authorized_user?(payload),
+         {:ok, %Houses.House{} = house} <- get_house(house_id, :product_lists),
+         %Houses.House{product_lists: product_lists} <- house do
       socket = assign(socket, :product_lists, product_lists)
-      response = %{product_lists: product_lists}
+      response = %{house: house, product_lists: product_lists}
       {:ok, response, socket}
     else
-      {:error, %{reason: "unauthorized"}}
+      {:error, error} -> {:reply, {:error, error}, socket}
     end
-  end
-
-  # Channels can be used in a request/response fashion
-  # by sending replies to requests from the client
-  @impl true
-  def handle_in("ping", payload, socket) do
-    {:reply, {:ok, payload}, socket}
-  end
-
-  # It is also common to receive messages from the client and
-  # broadcast to everyone in the current topic (house:lobby).
-  @impl true
-  def handle_in("shout", payload, socket) do
-    broadcast(socket, "shout", payload)
-    {:noreply, socket}
   end
 
   @impl true
   def handle_in("product_list:create", %{"product_list" => params}, socket) do
     params = Params.filter_params(params, @product_list_create_attrs)
 
-    case create_product_list(params) do
+    case Products.create_product_list(params) do
       {:ok, product_list} ->
         broadcast(socket, "product_list:created", %{product_list: product_list})
         {:noreply, socket}
 
-      {:error, error} ->
-        {:reply, {:error, error}, socket}
+      {:error, changeset} ->
+        {:reply, {:error, changeset.errors}, socket}
     end
   end
 
@@ -58,23 +65,91 @@ defmodule BaggyBackendWeb.HouseChannel do
     params = Map.take(params, @product_list_update_attrs)
 
     with {:ok, %ProductList{} = product_list} <- get_product_list(id),
-         {:ok, %ProductList{} = product_list} <- update_product_list(product_list, params) do
-      broadcast(socket, "product_list:updated", %{product_list: product_list})
+         {:ok, %ProductList{} = product_list} <-
+           Products.update_product_list(product_list, params) do
+      broadcast(socket, "product_list:update", %{product_list: product_list})
       {:noreply, socket}
     else
+      {:error, %Ecto.Changeset{} = changeset} -> {:reply, {:error, changeset.errors}, socket}
       {:error, error} -> {:reply, {:error, error}, socket}
     end
   end
 
-  # Add authorization logic here as required.
-  defp authorized?(_payload) do
-    true
+  @impl true
+  def handle_in(
+        "product_list:product:create",
+        %{"product_list_id" => product_list_id, "product" => params},
+        socket
+      ) do
+    params = Map.take(params, @product_create_attrs)
+
+    with {:ok, %ProductList{} = product_list} <- get_product_list(product_list_id),
+         {:ok, %Product{} = product} <-
+           Products.create_product(params) do
+      broadcast(socket, "product_list:product:create", %{
+        product_list_id: product_list.id,
+        product: product
+      })
+
+      {:noreply, socket}
+    else
+      {:error, %Ecto.Changeset{} = changeset} -> {:reply, {:error, changeset.errors}, socket}
+      {:error, error} -> {:reply, {:error, error}, socket}
+    end
   end
 
-  defp create_product_list(params) do
-    case Products.create_product_list(params) do
-      {:ok, %ProductList{} = product_list} -> {:ok, product_list}
-      {:error, changeset} -> {:error, changeset.errors}
+  @impl true
+  def handle_in(
+        "product_list:product:update",
+        %{"id" => id, "product" => params},
+        socket
+      ) do
+    params = Map.take(params, @product_update_attrs)
+
+    with {:ok, %Product{} = product} <- get_product(id),
+         {:ok, %Product{} = product} <- Products.update_product(product, params) do
+      broadcast(socket, "product_list:product:update", %{
+        product_list_id: product.product_list_id,
+        product: product
+      })
+
+      {:noreply, socket}
+    else
+      {:error, %Ecto.Changeset{} = changeset} -> {:reply, {:error, changeset.errors}, socket}
+      {:error, error} -> {:reply, {:error, error}, socket}
+    end
+  end
+
+  @impl true
+  def handle_in(
+        "product_list:product:delete",
+        %{"id" => id},
+        socket
+      ) do
+    with {:ok, %Product{} = product} <- get_product(id),
+         {:ok, %Product{}} <- Products.delete_product(product) do
+      broadcast(socket, "product_list:product:delete", %{
+        product_list_id: product.product_list_id,
+        product_id: id
+      })
+
+      {:noreply, socket}
+    else
+      {:error, %Ecto.Changeset{} = changeset} -> {:reply, {:error, changeset.errors}, socket}
+      {:error, error} -> {:reply, {:error, error}, socket}
+    end
+  end
+
+  defp authorized_user?(_payload) do
+    # Check correct passcode and search for houses_users association
+    if true, do: true, else: {:error, "unauthorized"}
+  end
+
+  defp get_house(id, assocs) do
+    try do
+      {:ok, Houses.get_house!(id, assocs)}
+    rescue
+      _error in Ecto.NoResultsError -> {:error, "Record with id #{id} could not be found"}
     end
   end
 
@@ -86,10 +161,11 @@ defmodule BaggyBackendWeb.HouseChannel do
     end
   end
 
-  defp update_product_list(product_list, params) do
-    case Products.update_product_list(product_list, params) do
-      {:ok, %ProductList{} = product_list} -> {:ok, product_list}
-      {:error, changeset} -> {:error, changeset.errors}
+  defp get_product(id) do
+    try do
+      {:ok, Products.get_product!(id)}
+    rescue
+      _error in Ecto.NoResultsError -> {:error, "Record with id #{id} could not be found"}
     end
   end
 end
