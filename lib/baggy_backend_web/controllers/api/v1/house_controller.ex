23b49692e8025677f7e63fc9c8f4b9b554afbcfe
alexander-p30 <alexanderp30e@gmail.com>
2021-01-31 19:25:45 -0300

    Adds auth requirement logic to house_controller
    Co-authored-by: PedroAugustoRamalhoDuarte <pedro_aduarte@aluno.unb.br>

--- lib/baggy_backend_web/controllers/api/v1/house_controller.ex
+++ lib/baggy_backend_web/controllers/api/v1/house_controller.ex
@@ -2,17 +2,23 @@ defmodule BaggyBackendWeb.Api.V1.HouseController do
   use BaggyBackendWeb, :controller
 
   alias BaggyBackend.Houses
-  alias BaggyBackend.Houses.House
+  alias BaggyBackend.Houses.{House, HousesUsers}
 
   action_fallback BaggyBackendWeb.FallbackController
 
-  def index(conn, %{"uuid" => user_uuid}) do
-    houses = Houses.list_houses(user_uuid)
+  def index(conn, _params) do
+    houses = Houses.list_houses(current_user(conn))
     render(conn, "index.json", houses: houses)
   end
 
   def create(conn, %{"house" => house_params}) do
-    with {:ok, %House{} = house} <- Houses.create_house(house_params) do
+    with {:ok, %House{} = house} <- Houses.create_house(house_params),
+         {:ok, %HousesUsers{}} <-
+           Houses.create_houses_users(%{
+             house_id: house.id,
+             user_uuid: current_user(conn).uuid,
+             is_owner: true
+           }) do
       conn
       |> put_status(:created)
       |> put_resp_header("location", Routes.api_v1_house_path(conn, :show, house))
@@ -22,13 +28,17 @@ defmodule BaggyBackendWeb.Api.V1.HouseController do
 
   def show(conn, %{"id" => id}) do
     house = Houses.get_house!(id)
-    render(conn, "show.json", house: house)
+
+    with :ok <- require_house_association(house, current_user(conn)) do
+      render(conn, "show.json", house: house)
+    end
   end
 
   def update(conn, %{"id" => id, "house" => house_params}) do
     house = Houses.get_house!(id)
 
-    with {:ok, %House{} = house} <- Houses.update_house(house, house_params) do
+    with :ok <- require_house_association(house, current_user(conn), require_is_owner: true),
+         {:ok, %House{} = house} <- Houses.update_house(house, house_params) do
       render(conn, "show.json", house: house)
     end
   end
@@ -36,8 +46,15 @@ defmodule BaggyBackendWeb.Api.V1.HouseController do
   def delete(conn, %{"id" => id}) do
     house = Houses.get_house!(id)
 
-    with {:ok, %House{}} <- Houses.delete_house(house) do
+    with :ok <- require_house_association(house, current_user(conn), require_is_owner: true),
+         {:ok, %House{}} <- Houses.delete_house(house) do
       send_resp(conn, :no_content, "")
     end
+  end
+
+  defp require_house_association(house, user, opts \\ [require_is_owner: false]) do
+    if house |> Houses.House.member?(user, opts[:require_is_owner]),
+      do: :ok,
+      else: {:error, :unauthorized, "Current user is not house owner."}
   end
 end
